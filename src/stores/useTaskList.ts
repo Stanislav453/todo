62379954b4e4143f562f012e09396551c1b4e1ce
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-07-30 21:43:45 +0200

    add CRUD function

--- src/stores/useTaskList.ts
+++ src/stores/useTaskList.ts
@@ -3,12 +3,32 @@ import { OneTaskType } from "../type";
 
 type UseTaskListType = {
   userTaskList: OneTaskType[];
-  updateUserTaskList: (data: OneTaskType) => void;
+  setTaskList: (data: OneTaskType[]) => void;
+  updateFilterTaskList: (number: string) => void;
+  updateTaskList: (data: OneTaskType) => void;
+  toggleTaskDone: (id: string) => void;
 };
 
 export const useTaskList = create<UseTaskListType>()((set) => ({
   userTaskList: [],
 
-  updateUserTaskList: (data) =>
-    set((state) => ({ userTaskList: [...state.userTaskList, data] })),
+  setTaskList: (data) =>
+    set(() => ({
+      userTaskList: data,
+    })),
+
+  updateFilterTaskList: (id) =>
+    set((state) => ({
+      userTaskList: state.userTaskList.filter((task) => task.id !== id),
+    })),
+  updateTaskList: (data) =>
+    set((state) => ({
+      userTaskList: [...state.userTaskList, data],
+    })),
+  toggleTaskDone: (id) =>
+    set((state) => ({
+      userTaskList: state.userTaskList.map((oneTask) =>
+        oneTask.id === id ? { ...oneTask, isDone: !oneTask.isDone } : oneTask
+      ),
+    })),
 }));
