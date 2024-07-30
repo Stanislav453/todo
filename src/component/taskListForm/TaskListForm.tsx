c51615af0012aed06ef62c632df2db98a01500e5
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-07-30 21:43:19 +0200

    create post to task, delete edit mod

--- src/component/taskListForm/TaskListForm.tsx
+++ src/component/taskListForm/TaskListForm.tsx
@@ -1,45 +1,58 @@
 import { useState } from "react";
 import { registerLocale } from "react-datepicker";
 import { CustomButton } from "../CustomButton";
-import "./date-picker.css";
 import { OneTaskType } from "../../type";
 import { es } from "date-fns/locale/es";
 import { useTaskList } from "../../stores/useTaskList";
-import { API_URL } from "../../api/url";
+import { TASKS_API_URL } from "../../api/url";
 import { isUserLogIn } from "../../stores/localStorage/isUserLogIn";
+// import { useTaskList } from "../../stores/useTaskList";
 
 registerLocale("es", es);
 
 export const TaskListForm = () => {
-  const [editIndex, setEditIndex] = useState<number | null>(null);
-  const setTaskList = useTaskList((state) => state.updateUserTaskList);
+  // const [editIndex, setEditIndex] = useState<number | null>(null);
+  const setTaskList = useTaskList((state) => state.updateTaskList);
   const [userTask, setUserTask] = useState<OneTaskType>({
+    id: "",
     taskName: "",
     isPriority: false,
     tag: "home",
     isDone: false,
+    listName: "listName 2",
+    userId: isUserLogIn.id,
   });
-
-  const USER_ID = 1
 
   const formSubmit = async () => {
     setTaskList(userTask);
 
     try {
-      const response = await fetch(`${API_URL}users/${USER_ID}`, {
-        method: "PUT",
+      const response = await fetch(`${TASKS_API_URL}`, {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          taskList: [userTask],
+          taskName: userTask.taskName,
+          isPriority: userTask.isPriority,
+          tag: userTask.tag,
+          isDone: false,
+          listName: "listName 2",
+          userId: isUserLogIn.id,
         }),
       });
       if (response.ok) {
-        // setSuccess(succesfull);
+        setUserTask({
+          id: "",
+          taskName: "",
+          isPriority: false,
+          tag: "home",
+          isDone: false,
+          listName: "listName 2",
+          userId: isUserLogIn.id,
+        });
         console.log("huraa");
       } else {
-        // setError(failed);
         console.log("noooo");
       }
     } catch (e) {
@@ -70,9 +83,9 @@ export const TaskListForm = () => {
     <form className="w-full flex flex-col justify-center items-center gap-1 mx-auto ">
       <div className="flex w-full">
         <input
-          className={`flex-1 bg-gray-50 border ${
-            editIndex === null ? "border-gray-300" : "border-orange-300"
-          } text-gray-900 text-sm rounded-tl-lg rounded-bl-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+          className={`flex-1 bg-gray-50 border 
+           border-gray-300
+          text-gray-900 text-sm rounded-tl-lg rounded-bl-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
           type="text"
           name="taskName"
           placeholder="Write your task"
@@ -82,15 +95,13 @@ export const TaskListForm = () => {
         <CustomButton
           buttonName="Add task"
           customStyle="bg-formButton"
-          action={() => formSubmit()}
+          action={() => {
+            formSubmit();
+          }}
         />
       </div>
       <div className="flex flex-wrap items-center  w-full gap-1">
-        <div
-          className={`flex gap-1 ${
-            editIndex === null ? "bg-gray-100" : "bg-orange-500"
-          } p-1 border-1  rounded-lg `}
-        >
+        <div className={`flex gap-1 "bg-gray-100 p-1 border-1  rounded-lg `}>
           <input
             type="checkbox"
             name="isPriority"
@@ -104,10 +115,7 @@ export const TaskListForm = () => {
           name="tag"
           onChange={handleChange}
           value={userTask.tag}
-          className={`flex-none w-26 p-2 rounded-lg                ${
-            editIndex === null ? "bg-gray-100" : "bg-orange-500"
-          }
- text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+          className={`flex-none w-26 p-2 rounded-lg bg-gray-100 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
         >
           <option value="home">Home</option>
           <option value="jobs">Jobs</option>
