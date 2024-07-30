bf70007a8db8cd9638b4e2fc7edf96e64c7402a0
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-07-30 21:44:44 +0200

    add api variable

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,1 +1,3 @@
-export const API_URL = "https://66a35f4944aa63704580f0a4.mockapi.io/api/todo/";
+export const USERS_API_URL = "https://66a35f4944aa63704580f0a4.mockapi.io/api/todo/";
+export const TASKS_API_URL =
+  "https://66a35f4944aa63704580f0a4.mockapi.io/api/todo/tasks";
