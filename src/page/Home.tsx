016eb48f47a8d5e043d9e72871101dcf2e159444
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-07-30 21:44:29 +0200

    delete unused code

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import { TaskListForm } from '../component/taskListForm/TaskListForm'
-import { TaskItem } from '../component/TaskItem';
+import React from "react";
+import { TaskListForm } from "../component/taskListForm/TaskListForm";
+import { TaskItem } from "../component/TaskItem";
+// import { useTaskList } from "../../stores/useTaskList";
+import { useTaskList } from "../stores/useTaskList";
 
 export const Home = () => {
+  const taskList = useTaskList((state) => state.userTaskList);
 
+  console.log("This is tasks from home", taskList);
 
   return (
     <main className="w-full max-w-5xl h-full mx-auto min-h-screen pt-10 sm:pt-16  px-5 ">
@@ -14,4 +18,4 @@ export const Home = () => {
       <TaskItem />
     </main>
   );
-}
+};
