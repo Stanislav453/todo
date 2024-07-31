b3e141caa49f74ca237d09634aca45a2a14f5730
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-07-31 04:43:52 +0200

    Create task List

--- src/api/actions/useFetchTask.ts
+++ src/api/actions/useFetchTask.ts
@@ -2,9 +2,11 @@ import { useEffect } from "react";
 import { USERS_API_URL } from "../url";
 import { isUserLogIn } from "../../stores/localStorage/isUserLogIn";
 import { useTaskList } from "../../stores/useTaskList";
+import { OneTaskType } from "../../type";
 
 export const useFetchTask = () => {
   const setTaskList = useTaskList((state) => state.setTaskList);
+  const setTaskListName = useTaskList((state) => state.setTaskListName);
 
   useEffect(() => {
     const fetchTasks = async () => {
@@ -20,8 +22,12 @@ export const useFetchTask = () => {
         );
 
         if (taskResponse.ok) {
-          const tasks = await taskResponse.json();
+          const tasks: OneTaskType[] = await taskResponse.json();
+          const taskListName = [
+            ...new Set(tasks.map((oneTask) => oneTask.listName)),
+          ];
           setTaskList(tasks);
+          setTaskListName(taskListName);
         } else {
           console.log("Nepodařilo se načíst úkoly");
         }
